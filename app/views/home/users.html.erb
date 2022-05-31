ea729e0ab47006563e8fac11e21030310d7f4e23
Dagi <dagi.zewdu.dz@gmail.com>
2022-05-31 18:18:36 +0300

    Added basic routing

---
+++ app/views/home/users.html.erb
@@ -0,0 +1,1 @@
+<h1>users route</h1>
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  # get 'home/index'
   root 'home#index'
+  get 'users/:user_id/posts'
+  get 'users/:user_id/posts/:id'
+  get 'users/'
+  get 'users/:id'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
