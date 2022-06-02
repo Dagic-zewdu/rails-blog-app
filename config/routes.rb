fa5e86a1c591d01c938afd5756b2906b5946f29f
Dagi <dagi.zewdu.dz@gmail.com>
2022-06-02 12:38:40 +0300

    Added postgres configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'users#index'
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
-  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
