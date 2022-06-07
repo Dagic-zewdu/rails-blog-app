61d895726f59ee99f40726a82cc0ac64f361c5a6
Dagi <dagi.zewdu.dz@gmail.com>
2022-06-07 13:12:26 +0300

    Fixed lintor errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def current_user
+    User.first
+  end
+
   private
 
   # Use callbacks to share common setup or constraints between actions.
